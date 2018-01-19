38873b1b54c87e018e0e53184f5f5940e773fa68
mayanje <jean.mayan@externe.e-i.com>
2018-01-19 10:28:52 +0100

    #22 : First implementation of the Message class.

---
+++ Solution/LanguageServer.Robot.Common/Model/Message.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageServer.Robot.Common.Model
+{
+    /// <summary>
+    /// A monotoring message
+    /// </summary>
+    public abstract class Message
+    {
+        public enum MessageKind
+        {
+            //A LSR or LSRM Command
+            Command,
+            //A LSP message:
+            Lsp,
+            //A Message Acknowledgment
+            Acknowledgment
+        }
+
+        /// <summary>
+        /// Message Kind constructor
+        /// </summary>
+        /// <param name="kind">Message's kind</param>
+        protected Message(MessageKind kind)
+        {
+            Kind = kind;
+        }
+
+        /// <summary>
+        /// Message's Kind.
+        /// </summary>
+        public MessageKind Kind
+        {
+            get;
+            private set;
+        }
+        /// <summary>
+        /// A LSP Message
+        /// </summary>
+        public class LspMessage : Message
+        {
+            /// <summary>
+            /// From who the message came.
+            /// </summary>
+            public enum MessageFrom
+            {
+                /// <summary>
+                /// A message sent by the client
+                /// </summary>
+                Client,
+                /// <summary>
+                /// A Message received from the server
+                /// </summary>
+                Server,
+            }
+
+            /// <summary>
+            /// Empty constructor
+            /// </summary>
+            public LspMessage() : base(MessageKind.Lsp)
+            {
+
+            }
+
+            /// <summary>
+            /// From message constructor
+            /// </summary>
+            /// <param name="from"></param>
+            /// <param name="message"></param>
+            public LspMessage(MessageFrom from, string message) : base(MessageKind.Lsp)
+            {
+                From = from;
+                Message = message;
+            }
+            /// <summary>
+            /// The LSP Message
+            /// </summary>
+            public string Message
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// LSP Message's origin
+            /// </summary>
+            public MessageFrom From
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
